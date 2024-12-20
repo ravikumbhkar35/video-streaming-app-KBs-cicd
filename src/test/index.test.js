dd138281c0ccf28d85d5711d0e1dd90766cf774a
ravikumbhkar35 <ravikumbhkar35@gmail.com>
2024-12-20 11:53:28 +0530

    2bd commit

---
+++ src/test/index.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest')
+
+const {app} = require('../index')
+
+describe("video streaming service", () =>{
+
+    test("service can handle request ", async ()=>{
+        const response = await request(app).get("/video")
+        expect(response.status).toBe(200)
+    })
+}) 
